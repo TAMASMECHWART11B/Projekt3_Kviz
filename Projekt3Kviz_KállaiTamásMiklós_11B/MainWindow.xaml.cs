963bba64fc0932b0e4a1b469631202c92439965f
TAMASMECHWART11B <kallaitamas04@gmail.com>
2022-02-28 22:46:13 +0100

    Betölt
    betölt egy tantárgyat és egy témát a combo boxba

--- Projekt3Kviz_KállaiTamásMiklós_11B/MainWindow.xaml.cs
+++ Projekt3Kviz_KállaiTamásMiklós_11B/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,16 +22,52 @@ namespace Projekt3Kviz_KállaiTamásMiklós_11B
     public partial class MainWindow : Window
     {
         List<string> tantargyak = new List<string>();
-        List<string> temakor = new List<string>();
+        List<string> temakorok = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
-            ToriBeolvasas();
+            
+            TortenelemBetoltese();
+
+
+
         }
 
-        private void ToriBeolvasas()
+
+
+
+        private void TortenelemBetoltese()
         {
+            string[] tori = File.ReadAllLines("masodikvhtori.txt");
+            string vhtori = tori[0].Split(';')[0];
+
+            foreach (string sor in tori)
+            {
+                string tortenelem = sor.Split(';')[0];
+                tantargyak.Add(tortenelem);
+                tantargy.Items.Add(tortenelem);
+                break;
+            }
+            string[] masodikvh = File.ReadAllLines("masodikvhtori.txt");
+            string magyarTori = masodikvh[0].Split(';')[0];
+            foreach (string sor in masodikvh)
+            {
+                string magyarTori1 = sor.Split(';')[1];
+                temakor.Items.Add(magyarTori1);
+
+                if (tantargy.SelectedItem == masodikvh)
+                {
+                    temakor.Items.Add(magyarTori1);
+                }
+                break;
+            }
+
             
+        }
+
+        private void kilepes_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ha erre a gombra nyomott Tanár úr, az valószínüleg azért van, mert nem értékelte túl nagy értékűre a projektemet. Kérem gondolja újra az érdemjegyet, hátha többre tudja majd Tanár úr értékelni:)");
         }
     }
 }
