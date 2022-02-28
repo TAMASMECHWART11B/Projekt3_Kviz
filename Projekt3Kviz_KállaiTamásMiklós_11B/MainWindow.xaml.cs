62d8608ea6851ed7ab51552b7de07fe5a7eda47a
TAMASMECHWART11B <kallaitamas04@gmail.com>
2022-02-28 18:57:29 +0100

    XMAL kész
    Az alap xmal kész, kérdések gyűjtése folyamatban

---
+++ Projekt3Kviz_KállaiTamásMiklós_11B/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt3Kviz_KállaiTamásMiklós_11B
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> tantargyak = new List<string>();
+        List<string> temakor = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ToriBeolvasas();
+        }
+
+        private void ToriBeolvasas()
+        {
+            
+        }
+    }
+}
